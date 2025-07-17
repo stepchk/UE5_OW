7fcffe61ada65728c75f667dc761b0018ef359a5
stepchk <108991776+stepchk@users.noreply.github.com>
2025-07-17 12:00:11 +0300

    add

---
+++ Source/MyProject1/Private/Items/Item0.cpp
@@ -0,0 +1,81 @@
+ // Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Item0.h"
+#include "MyProject1/DebugMacros.h"
+#include "Components/SphereComponent.h"
+#include "MyCharacter.h"
+#include "NiagaraComponent.h"
+
+// Sets default values
+AItem0::AItem0()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Item Mesh Component"));
+	ItemMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	ItemMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = ItemMesh;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+	Sphere->SetupAttachment(GetRootComponent());
+
+	EmbersEffect = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Embers"));
+	EmbersEffect->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void AItem0::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AItem0::OnSphereOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AItem0::OnSphereEndOverlap);
+}
+
+float AItem0::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem0::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
+void AItem0::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	 
+	 AMyCharacter* MyCharacter = Cast<AMyCharacter>(OtherActor);
+	 if (MyCharacter) 
+	 { 
+		 MyCharacter->SetOverlappingItem(this);
+	 }
+}
+
+void AItem0::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	AMyCharacter* MyCharacter = Cast<AMyCharacter>(OtherActor);
+	if (MyCharacter)
+	{
+		MyCharacter->SetOverlappingItem(nullptr);
+	}
+
+}
+
+// Called every frame
+void AItem0::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	RunningTime += DeltaTime;
+
+	if (ItemState == EItemState::EIS_Hovering)
+	{
+		AddActorWorldOffset(FVector(0.f, 0.f, TransformedSin()));
+	}
+
+	
+	
+	
+}
---
+++ Source/MyProject1/Public/Breakble/BreakbleActor.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/HitInterface.h"
+#include "BreakbleActor.generated.h"
+
+class UGeometryCollectionComponent;
+
+UCLASS()
+class MYPROJECT1_API ABreakbleActor : public AActor, public IHitInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	
+	ABreakbleActor();
+	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void GetHit_Implementation(const FVector& ImpactPoint) override;
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UGeometryCollectionComponent* GeometryCollection;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UCapsuleComponent* Capsule;
+
+private:	
+	
+	
+	UPROPERTY(EditAnywhere,Category = "Breakble Properties")
+	TArray<TSubclassOf<class ATreasure>> TreasureClasses;
+	
+	bool bBroken = false;
+
+};
